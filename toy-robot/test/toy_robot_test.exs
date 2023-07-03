0a7d80261b2511c7e66d4e13708127fb6933410d
Chris Keele <christhekeele@gmail.com>
2023-07-03 18:55:24 -0500

    Update toy-robot test suite.

--- toy-robot/test/toy_robot_test.exs
+++ toy-robot/test/toy_robot_test.exs
@@ -6,28 +6,48 @@ defmodule ToyRobotTest do
 
   test "Example A" do
     assert simulation_result("""
-      PLACE 0,0,NORTH
-      MOVE
-      REPORT
-    """) == "0,1,NORTH"
+             PLACE 0,0,NORTH
+             MOVE
+             REPORT
+           """) == "0,1,NORTH"
   end
 
   test "Example B" do
     assert simulation_result("""
-      PLACE 0,0,NORTH
-      LEFT
-      REPORT
-    """) == "0,0,WEST"
+             PLACE 0,0,NORTH
+             LEFT
+             REPORT
+           """) == "0,0,WEST"
   end
 
   test "Example C" do
     assert simulation_result("""
-      PLACE 1,2,EAST
-      MOVE
-      MOVE
-      LEFT
-      MOVE
+             PLACE 1,2,EAST
+             MOVE
+             MOVE
+             LEFT
+             MOVE
+             REPORT
+           """) == "3,3,NORTH"
+  end
+
+  test "walking to the edge of the playing field without going over" do
+    assert simulation_result("""
+           PLACE 3,3,EAST
+           MOVE
+           MOVE
+           RIGHT
+           MOVE
+           REPORT
+           """) == "4,2,SOUTH"
+  end
+
+  test "cannot make invalid placement" do
+    assert_raise FunctionClauseError, fn ->
+      simulation_result("""
+      PLACE 3,6,EAST
       REPORT
-    """) == "3,3,NORTH"
+      """) == "4,2,SOUTH"
+    end
   end
 end
